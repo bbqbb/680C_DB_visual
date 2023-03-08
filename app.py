e9cfd6d811c519023279d1e56a6abb257331afef
bbqbb <kc.bbqbb@gmail.com>
2023-03-08 15:35:29 -0600

    update chart

--- app.py
+++ app.py
@@ -3,17 +3,21 @@ from dash import dcc, html, Dash
 from main import yyyy
 import pandas as pd
 from dash.dependencies import Output, Input
+import openpyxl
 
-data = pd.read_excel("T-Shirt.HD.xlsx", sheet_name="Follow-up")
-data['Date'] = data['Date'].str.extract(r'(\d{1,2}\.\d{1,2}\.\d{2,4})')
-data['Date'] = data['Date'].apply(yyyy)
-data['Date'] = pd.to_datetime(data['Date'], format="%m.%d.%Y", errors='coerce')
+# data = pd.read_excel("T-Shirt.HD.xlsx", sheet_name="Trial-Run")
+# data['Date'] = data['Date'].str.extract(r'(\d{1,2}\.\d{1,2}\.\d{2,4})')
+# data['Date'] = data['Date'].apply(yyyy)
+# data['Date'] = pd.to_datetime(data['Date'], format="%m.%d.%Y", errors='coerce')
+# data.sort_values("Date", inplace=True)
+data = pd.read_csv("avocado.csv")
+data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
 
 external_stylesheets = [
     {
         "href": "https://fonts.googleapis.com/css2?"
                 "family=Lato:wght@400;700&display=swap",
-
         "rel": "stylesheet",
     }
 ]
@@ -27,7 +31,12 @@ app.layout = html.Div(
             html.A(html.Img(src="/assets/inteplast.png"),
                    href="https://www.inteplast.com/"),
             html.H1("Technical Data"),
-            dcc.DatePickerRange(id="date_range")
+            dcc.DatePickerRange(id="date_range",
+                                min_date_allowed=data.Date.min().date(),
+                                max_date_allowed=data.Date.max().date(),
+                                start_date=data.Date.min().date(),
+                                end_date=data.Date.max().date(),
+                                )
         ]
         )
     ]
@@ -36,15 +45,45 @@ app.layout = html.Div(
             children=[
                 html.Div(
                     children=[
-                        dcc.Graph(id="chart"),
+                        dcc.Graph(id="chart", config={"displayModeBar": False}),
                     ], className="card"
                 )
             ], className="wrapper"
         )
     ]
 )
-@app.callback()
-    [Output("chart")]
+
+
+@app.callback(
+    [Output("chart", "figure")],
+    [Input("date_range", "start_date"),
+     Input("date_range", "end_date")]
+)
+def update_chart(start_date, end_date):
+    mask = ((data.Date >= start_date) & (data.Date <= end_date))
+    filter_data = data.loc[mask]
+    property_chart = {
+        "data": [
+            {
+                "x": filter_data["Date"],
+                "y": filter_data["AveragePrice"],
+                "type": "lines",
+                "hovertemplate": "$%{y:.2f}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "Average Price of Avocados",
+                "x": 0.5,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "$", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+    return property_chart
+
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=True, port=1234)
--- scratch.py
+++ scratch.py
@@ -1,24 +1,34 @@
 import pandas as pd
+from main import yyyy
+#
+# df = pd.DataFrame({
+#     'Date': ['01.01.22', '01.01.2022', '12.31.2021', '1.1.22', '31.12.2021', '31.12.2021-1234', "2.1"]
+# })
+# df['Date'] = df['Date'].str.extract(r'(\d{1,2}\.\d{1,2}\.\d{2,4})')
+#
+#
+# # df['Date'] = pd.to_datetime(df['Date'], format="%m.%d.%Y", errors='coerce')
+# def yyyy(y):
+#     y = str(y)
+#     parts = y.split(".")
+#     if len(parts) == 3 and len(parts[2]) == 2:
+#         y = y.split(".")
+#         y[2] = "20" + y[2]
+#         y = ".".join(y)
+#         return y
+#     else:
+#         return y
+#
+#
+# df['Date'] = df['Date'].apply(yyyy)
+#
+# print(df['Date'])
 
-df = pd.DataFrame({
-    'Date': ['01.01.22', '01.01.2022', '12.31.2021', '1.1.22', '31.12.2021', '31.12.2021-1234', "2.1"]
-})
-df['Date'] = df['Date'].str.extract(r'(\d{1,2}\.\d{1,2}\.\d{2,4})')
+data = pd.read_excel("T-Shirt.HD - Copy.csv", sheet_name="Follow-up")
+data['Date'] = data['Date'].str.extract(r'(\d{1,2}\.\d{1,2}\.\d{2,4})')
+data['Date'] = data['Date'].apply(yyyy)
+data['Date'] = pd.to_datetime(data['Date'], format="%m.%d.%Y", errors='coerce')
+data.sort_values("Date", inplace=True)
+data['Date'].dropna(inplace=True)
 
-
-# df['Date'] = pd.to_datetime(df['Date'], format="%m.%d.%Y", errors='coerce')
-def yyyy(y):
-    y = str(y)
-    parts = y.split(".")
-    if len(parts) == 3 and len(parts[2]) == 2:
-        y = y.split(".")
-        y[2] = "20" + y[2]
-        y = ".".join(y)
-        return y
-    else:
-        return y
-
-
-df['Date'] = df['Date'].apply(yyyy)
-
-print(df['Date'])
+print(data)
